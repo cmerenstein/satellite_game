7f99f1e3095a6ff1f9f25ea7a702d94eba5ec7c3
Carter Merenstein <cmerenstein@gmail.com>
2017-12-12 18:26:23 -0500

    average miss and 5 per game

--- satellite_game/views.py
+++ satellite_game/views.py
@@ -5,13 +5,15 @@ import random
 
 # Create your views here.
 def game(request):
-	n_cities = len(City.objects.all())
-	i = round(random.random()*n_cities)
-	city = City.objects.get(id_num=i)
-	
-	dict = {"city_lat":city.lat, "city_lng":city.lng, "city_name": city.name}
-	
-	return render(request, 'game.html', dict)
+	cities = []
+	for j in range(5):
+		n_cities = len(City.objects.all())
+		i = round(random.random()*n_cities)
+		city = City.objects.get(id_num=i)
+		cities.append({'city_lat':city.lat, 'city_lng': city.lng, 'city_name':city.name})
+		
+	json_data = json.dumps(cities)
+	return render(request, 'game.html', {"data":json_data})
 	
 def index(request):
 	
